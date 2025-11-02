756a04c585e68fa5681b92a13bdc0b898f861040
nhart985 <66880006+nhart985@users.noreply.github.com>
2025-11-02 16:41:05 -0500

    Add files via upload
    Update function to match Figure 5 format

--- R/Compare_Models.R
+++ R/Compare_Models.R
@@ -1,23 +1,22 @@
+source('Find_C_SmoothC.R')
+library(sigmoid)
 #' compare_models()
-#' @description
-#' Allows the user to easily compare the gains in concordance, using both
-#' traditional C-index and the proposed new Smooth C-index, between two nested
-#' survival models. Returns concordance values and generates side-by-side plots
-#' using these values as parameters to illustrate concordance gains.
-#'
-#' @param null.vars Vector of variable names for the null model
-#' @param added.vars Vector of variable names to be added to the null model to yield the full model
-#' @param time Character string. Name of the time-to-event variable
-#' @param status Character string. Name of the event status variable
-#' @param data Data frame containing time, status, and the variables specified in the null and full models
+#' @param null.vars
+#' @param added.vars
+#' @param time 
+#' @param status
+#' @param data
 #' @param weighted Boolean, default TRUE
 #' @param dept.censor Boolean, default FALSE
-#' @return Data frame of traditional C-indices and proposed new Smooth C-indices
-#' for both the null and full models.
-#'
-#' @importFrom sigmoid sigmoid
+#' @return Generates side-by-side plots comparing concordance gains by 
+#' traditional C-index and proposed new Smooth C-index, as well as:
+#'  * C.df a data frame of c-indices
 compare_models = function(null.vars, added.vars, time, status, data,
                           weighted = TRUE, dept.censor = FALSE) {
+  
+    library(ggplot2)
+    library(ggpubr)
+    
     ## Calculate and store concordance values
     null.form = paste0(
         "Surv(",
@@ -38,61 +37,53 @@ compare_models = function(null.vars, added.vars, time, status, data,
         weighted = weighted,
         dept.censor = dept.censor
     )
-
+    
     c.idx1 = cvals.null$C
     c.idx2 = cvals.full$C
     nu1 = cvals.null$smooth.C
     nu2 = cvals.full$smooth.C
-    C.df = data.frame(
-        null.model = c(c.idx1, nu1),
-        full.model = c(c.idx2, nu2),
-        row.names = c("C-index (trad)", "Smooth C-index")
-    )
-
-    ## Generate x & y values for C-index plot
-    ## y-values are binary, based on sign of difference
-    x.neg = c(-3, 0)
-    x.pos = c(0, 3)
-    y.neg1 = rep(1-c.idx1, 2)
-    y.pos1 = rep(c.idx1, 2)
-    y.neg2 = rep(1-c.idx2, 2)
-    y.pos2 = rep(c.idx2, 2)
-
-    ## Generate x-values for Smooth C plot
+    
+    C.df=data.frame(C=c(c.idx1,c.idx2),exp.nu=c(exp(nu1),exp(nu2)))
+    
+    df=data.frame(x=rep(c(-3,0,0,3),2),y=c(1-c.idx1,1-c.idx1,c.idx1,c.idx1,
+                                           1-c.idx2,1-c.idx2,c.idx2,c.idx2),
+                  type=rep(as.character(1:2),each=4),
+                  x0=rep(as.character(c(1,1,2,2)),2))
+    
+    g=ggplot(df)
+    g=g+geom_line(aes(x=x,y=y,linetype=type,colour=type,group=interaction(x0,type)),lwd=1.5)
+    g=g+scale_colour_manual(labels=c("Initial Model","Full Model"),values=c("dodgerblue3","navy"))
+    g=g+scale_linetype_manual(labels=c("Initial Model","Full Model"),values=c("dotted","solid"))
+    g=g+theme_classic()
+    g=g+theme(legend.position="top",legend.title=element_blank(),legend.key.width=unit(3,"cm"),text=element_text(size=24))
+    g=g+xlab(expression(paste(italic(R[i]-R[j])," (SD)")))
+    g=g+ylab(expression(italic(P(T[i] < T[j]))))
+    g=g+scale_x_continuous(breaks=seq(-3,3,1))
+    g=g+ylim(0,1)
+    g=g+ggtitle("(a)")
+  
     x.val = seq(-3, 3, by = 0.01)
-
-    ## Generate y-values for Smooth C plot using sigmoid function
+    
+    ## Generate y-values using sigmoid function
     y.val1 = sigmoid(nu1 * x.val)
     y.val2 = sigmoid(nu2 * x.val)
-
-
-    #### Plot concordance gains ####
-    par(mfrow = c(1, 2))
-    plot(
-        y.neg1 ~ x.neg,
-        type = "l",
-        lwd = 5,
-        ylab = "Probability T1 < T2",
-        xlab = "Difference in Risk Scores (SD)",
-        xlim = c(-3, 3),
-        ylim = c(0, 1),
-        main = "C-Index"
-    )
-    lines(y.pos1 ~ x.pos, type = "l", lwd = 5)
-    lines(y.neg2 ~ x.neg, type = "l", lwd = 5, col = 2)
-    lines(y.pos2 ~ x.pos, type = "l", lwd = 5, col = 2)
-    plot(
-        y.val1 ~ x.val,
-        type = "l",
-        lwd = 5,
-        ylab = "Probability T1 < T2",
-        xlab = "Difference in Risk Scores (SD)",
-        xlim = c(-3, 3),
-        ylim = c(0, 1),
-        main = "Smooth C-Index"
-    )
-    lines(y.val2 ~ x.val, type = "l", lwd = 5, col = 2)
-
+    
+    df=data.frame(x=rep(x.val,2),y=c(y.val1,y.val2),nu=rep(as.character(1:2),each=length(x.val)))
+    g2=ggplot(df)
+    g2=g2+geom_line(aes(x=x,y=y,linetype=nu,colour=nu),lwd=1.5)
+    g2=g2+scale_colour_manual(labels=c("Initial Model","Full Model"),values=c("dodgerblue3","navy"))
+    g2=g2+scale_linetype_manual(labels=c("Initial Model","Full Model"),values=c("dotted","solid"))
+    g2=g2+theme_classic()
+    g2=g2+theme(legend.position="top",legend.key.width=unit(3,"cm"),text=element_text(size=24),legend.text=element_text(size=24),legend.title=element_blank())
+    g2=g2+xlab(expression(paste(italic(R[i]-R[j])," (SD)")))
+    g2=g2+ylab("")
+    g2=g2+scale_x_continuous(breaks=seq(min(x.val),max(x.val),1))
+    g2=g2+ylim(0,1)
+    g2=g2+ggtitle("(b)")
+    
+    out=ggarrange(g,g2,nrow=1,common.legend=T)
+    
     ## Finally, return data frame of values
-    return(C.df)
+    return(list(C.df,out))
 }
+
